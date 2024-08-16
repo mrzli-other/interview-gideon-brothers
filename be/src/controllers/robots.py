c1b3ba9a0b9b744796b571fc9f4afe3b5b220948
Goran Mržljak <goran.mrzljak@gmail.com>
2024-08-16 22:12:06 +0200

    implement RobotService and wire up Robot resources to database

--- be/src/controllers/robots.py
+++ be/src/controllers/robots.py
@@ -2,12 +2,12 @@ from flask_restful import Resource
 from flask_apispec import MethodResource, doc, use_kwargs, marshal_with
 from marshmallow import Schema, fields, validate
 
-robots = []
-
 class RobotSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True, validate=validate.Length(min=1, max=255))
     robot_type_id = fields.Int(required=True)
+    created_at = fields.Str(dump_only=True)
+    updated_at = fields.Str(dump_only=True)
 
 class Robots(MethodResource, Resource):
     def __init__(self, container):
@@ -17,18 +17,14 @@ class Robots(MethodResource, Resource):
     @doc(description='Get all robots', tags=['Robots'])
     @marshal_with(RobotSchema(many=True))
     def get(self):
+        robots = self.service.get_all()
         return robots, 200
 
     @doc(description='Create a new robot', tags=['Robots'])
     @use_kwargs(RobotSchema, location=('json'))
     @marshal_with(RobotSchema)
     def post(self, **kwargs):
-        robot = {
-            'id': len(robots) + 1,
-            'name': kwargs['name'],
-            'robot_type_id': kwargs['robot_type_id']
-        }
-        robots.append(robot)
+        robot = self.service.create(kwargs)
         return robot, 201
 
 class Robot(MethodResource, Resource):
@@ -39,23 +35,21 @@ class Robot(MethodResource, Resource):
     @doc(description='Get a robot by ID', tags=['Robots'])
     @marshal_with(RobotSchema)
     def get(self, id):
-        robot = next((robot for robot in robots if robot['id'] == id), None)
-        if robot:
-            return robot, 200
-        return {'message': 'Robot not found'}, 404
+        robot = self.service.get_one(id)
+        if not robot:
+            return {'message': 'Robot not found'}, 404
+        return robot, 200
 
     @doc(description='Update a robot by ID', tags=['Robots'])
     @use_kwargs(RobotSchema, location=('json'))
     @marshal_with(RobotSchema)
     def put(self, id, **kwargs):
-        robot = next((robot for robot in robots if robot['id'] == id), None)
-        if robot:
-            robot.update(kwargs)
-            return robot, 200
-        return {'message': 'Robot not found'}, 404
+        robot = self.service.update(id, kwargs)
+        if not robot:
+            return {'message': 'Robot not found'}, 404
+        return robot, 200
 
     @doc(description='Delete a robot by ID', tags=['Robots'])
     def delete(self, id):
-        global robots
-        robots = [robot for robot in robots if robot['id'] != id]
+        self.service.delete(id)
         return '', 204
--- be/src/services/robot_service.py
+++ be/src/services/robot_service.py
@@ -1,2 +1,35 @@
+from util.date import to_iso_utc
+from util.transform import transform_if_exists
+
 class RobotService:
-  pass
+  def __init__(self, container):
+    self.container = container
+    self.dao = container.get_robot_dao()
+
+  def get_all(self):
+    data = self.dao.fetch_all()
+    return [_tuple_to_dict(item) for item in data]
+  
+  def create(self, data):
+    data = self.dao.create(data['name'], data['robot_type_id'])
+    return _tuple_to_dict(data)
+  
+  def get_one(self, id):
+    data = self.dao.fetch_one(id)
+    return transform_if_exists(data, _tuple_to_dict)
+  
+  def update(self, id, data):
+    data = self.dao.update(id, data['name'], data['robot_type_id'])
+    return transform_if_exists(data, _tuple_to_dict)
+  
+  def delete(self, id):
+    self.dao.delete(id)
+
+def _tuple_to_dict(t):
+    return {
+        'id': t[0],
+        'name': t[1],
+        'robot_type_id': t[2],
+        'created_at': to_iso_utc(t[3]),
+        'updated_at': to_iso_utc(t[4]),
+    }
