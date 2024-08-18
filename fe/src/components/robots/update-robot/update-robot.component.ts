4723748510d9c459f370285ec9649e7545cc63b7
Goran Mržljak <goran.mrzljak@gmail.com>
2024-08-18 02:45:50 +0200

    invalid robot update component (basic)

--- fe/src/components/robots/update-robot/update-robot.component.ts
+++ fe/src/components/robots/update-robot/update-robot.component.ts
@@ -1,25 +1,102 @@
-import { AsyncPipe } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { map, Observable, of } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { combineLatest, map, Subscription } from 'rxjs';
+import { robotFeature, RobotActions } from '../../../store';
+import { Robot, RobotUpdate } from '../../../types';
+import { RmButtonComponent, RmTextInputComponent } from '../../shared';
+import { getIntParam, isFormFieldError } from '../../util';
+import { transformIfNotUndefined, parseIntOrThrow } from '../../../util';
 
 @Component({
   selector: 'update-robot',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [ReactiveFormsModule, RmButtonComponent, RmTextInputComponent],
   templateUrl: './update-robot.component.html',
 })
-export class UpdateRobotComponent implements OnInit {
-  public id$: Observable<number | undefined> = of(undefined);
+export class UpdateRobotComponent implements OnInit, OnDestroy {
+  public form: FormGroup = new FormGroup({});
 
-  public constructor(private readonly route: ActivatedRoute) {}
+  public robot: Robot | undefined = undefined;
+
+  private robotSubscription: Subscription | undefined = undefined;
+
+  public constructor(
+    private readonly route: ActivatedRoute,
+    private readonly store: Store,
+    private readonly fb: FormBuilder,
+  ) {}
 
   public ngOnInit(): void {
-    this.id$ = this.route.paramMap.pipe(
-      map((params) => {
-        const value = params.get('id') ?? undefined;
-        return value !== undefined ? Number.parseInt(value, 10) : undefined;
-      }),
+    this.robotSubscription = combineLatest([
+      this.route.paramMap,
+      this.store.select(robotFeature.selectRobots),
+    ])
+      .pipe(
+        map(([params, robots]) => {
+          const id = getIntParam(params, 'id');
+
+          if (id === undefined) {
+            return undefined;
+          }
+
+          return robots?.find((robot) => robot.id === id);
+        }),
+      )
+      .subscribe((robot: Robot | undefined) => {
+        this.robot = robot;
+
+        this.form = this.fb.group({
+          name: [
+            robot?.name ?? '',
+            [
+              Validators.required,
+              Validators.minLength(1),
+              Validators.maxLength(255),
+            ],
+          ],
+          robotTypeId: [
+            robot?.robotTypeId ?? '',
+            [Validators.required, Validators.pattern(/^[1-9]\d{0,10}$/)],
+          ],
+        });
+      });
+  }
+
+  public ngOnDestroy(): void {
+    this.robotSubscription?.unsubscribe();
+  }
+
+  public handleSubmit(): void {
+    if (this.robot === undefined || this.form.invalid) {
+      return;
+    }
+
+    const robotTypeId = transformIfNotUndefined(
+      this.form.value.robotTypeId ?? undefined,
+      parseIntOrThrow,
     );
+
+    if (robotTypeId === undefined) {
+      return;
+    }
+
+    const data: RobotUpdate = {
+      id: this.robot.id,
+      name: this.form.value.name ?? '',
+      robotTypeId,
+    };
+
+    this.store.dispatch(RobotActions.update({ payload: data }));
+  }
+
+  public isError(field: string): boolean {
+    return isFormFieldError(this.form, field);
   }
 }
