67dd6f546af2cc8f1ca81e87357c8f4b3284a2a9
Goran Mržljak <goran.mrzljak@gmail.com>
2024-08-18 02:39:23 +0200

    implement creating of robot (basic)

--- fe/src/components/robots/create-robot/create-robot.component.ts
+++ fe/src/components/robots/create-robot/create-robot.component.ts
@@ -1,9 +1,61 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RmButtonComponent, RmTextInputComponent } from '../../shared';
+import { Store } from '@ngrx/store';
+import { RobotActions } from '../../../store';
+import { RobotCreate } from '../../../types';
+import { isFormFieldError } from '../../util';
+import { parseIntOrThrow, transformIfNotUndefined } from '../../../util';
 
 @Component({
   selector: 'create-robot',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, RmButtonComponent, RmTextInputComponent],
   templateUrl: './create-robot.component.html',
 })
-export class CreateRobotComponent {}
+export class CreateRobotComponent {
+  public readonly form = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(1),
+      Validators.maxLength(255),
+    ]),
+    robotTypeId: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^[1-9]\d{0,10}$/),
+    ]),
+  });
+
+  public constructor(private readonly store: Store) {}
+
+  public handleSubmit(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const robotTypeId = transformIfNotUndefined(
+      this.form.value.robotTypeId ?? undefined,
+      parseIntOrThrow,
+    );
+
+    if (robotTypeId === undefined) {
+      return;
+    }
+
+    const data: RobotCreate = {
+      name: this.form.value.name ?? '',
+      robotTypeId,
+    };
+
+    this.store.dispatch(RobotActions.create({ payload: data }));
+  }
+
+  public isError(field: string): boolean {
+    return isFormFieldError(this.form, field);
+  }
+}
